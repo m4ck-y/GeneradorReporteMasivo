f7b286331a244674f819cb8e50b1fe0a7675e96c
m4ck-y <macario.alvaradohdez@gmail.com>
2025-02-25 10:22:40 -0600

    FEAT (BACKEND-API) - CORS

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from api.reporte import ReporteService
 from api.campania import CampaniaService
+from fastapi.middleware.cors import CORSMiddleware
 
 def setup_services(api_server: FastAPI):
     ReporteService(api_server)
     CampaniaService(api_server)
+
+    # Definir las configuraciones de CORS
+    api_server.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Permite todos los orígenes. Puedes poner una lista con orígenes específicos, por ejemplo: ["https://tusitio.com"]
+        allow_credentials=True,
+        allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+        allow_headers=["*"],  # Permite todos los encabezados
+    )
